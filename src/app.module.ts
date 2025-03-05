7bd2f5cfdb15dd5e7827d05d1d5aebbcc9fdc308
jiwon yang <ambosing_@naver.com>
2025-03-05 16:37:47 +0900

    Feat: Winston 로깅 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,28 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { WinstonModule } from 'nest-winston';
+import * as winston from 'winston';
 
 @Module({
-  imports: [],
+  imports: [WinstonModule.forRoot({
+    level: 'debug',
+    format: winston.format.combine(
+      winston.format.timestamp(),
+      winston.format.json(),
+    ),
+    transports: [
+      new winston.transports.Console({
+        format: winston.format.combine(
+          winston.format.colorize(),
+          winston.format.simple(),
+        ),
+      }),
+      new winston.transports.File({ filename: 'error.log', level: 'error' }),
+      new winston.transports.File({ filename: 'combined.log' })
+    ]
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
