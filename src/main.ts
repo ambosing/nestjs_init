9e4ece406fa9948d590583749a70b19d0ef86549
jiwon yang <ambosing_@naver.com>
2025-03-05 17:06:34 +0900

    Feat: Swagger 설정

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
-import { DocumentBuilder } from '@nestjs/swagger';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     bufferLogs: true,
   });
+  const documentOptions = new DocumentBuilder()
+    .setTitle('API Title')
+    .setDescription('API Description')
+    .setVersion('1.0')
+    .addServer('http://localhost:3000')
+    .addServer('https://staging.yourapi.com/', 'Staging')
+    .addServer('https://production.yourapi.com/', 'Production')
+    .addTag('API')
+    .build();
 
+  const document = SwaggerModule.createDocument(app, documentOptions);
+  SwaggerModule.setup('api-docs', app, document);
 
   app.useLogger(app.get(WINSTON_MODULE_NEST_PROVIDER));
 
