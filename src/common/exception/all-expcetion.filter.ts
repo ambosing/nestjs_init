fd143cb78f044e1abdc5546b81a0bb096abc603b
jiwon yang <ambosing_@naver.com>
2025-03-07 09:22:14 +0900

    Feat: Exception Filter 추가 구현

--- src/common/exception/all-expcetion.filter.ts
+++ src/common/exception/all-expcetion.filter.ts
@@ -9,11 +9,6 @@ export class AllExceptionsFilter extends BaseExceptionFilter {
   private readonly logger = new Logger(AllExceptionsFilter.name);
 
   catch(exception: HttpException, host: ArgumentsHost) {
-    const ctx = host.switchToHttp();
-    const response = ctx.getResponse<Response>();
-    const request = ctx.getRequest<Request>();
-    const status = exception.getStatus();
-
     if (exception instanceof Error) {
       const ctx = host.switchToHttp();
       const response = ctx.getResponse<Response>();
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import helmet from 'helmet';
+import { HttpExceptionFilter } from './common/exception/http-expcetion.filter';
+import { AllExceptionsFilter } from './common/exception/all-expcetion.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
@@ -24,6 +26,7 @@ async function bootstrap() {
 
   app.useLogger(app.get(WINSTON_MODULE_NEST_PROVIDER));
   app.use(helmet());
+  app.useGlobalFilters(new HttpExceptionFilter(), new AllExceptionsFilter());
 
   await app.listen(process.env.PORT || 3000);
 }
